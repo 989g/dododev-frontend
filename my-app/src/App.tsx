a4d6c1e892118763832a0193c4010cef8da5f8c9
989x <99growsup@gmail.com>
2024-03-21 11:53:23 +0700

    add tailwindcss

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 function App() {
   return (
     <div>
-      React
+      <h1 className='text-3xl font-bold underline text-blue-600'>Hello world!</h1>
     </div>
   );
 }
