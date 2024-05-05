0160673ec7e45f54468c0eefbb92ad95068f4ed2
989x <99growsup@gmail.com>
2024-05-06 00:11:57 +0700

    add components/Example

---
+++ my-app/src/components/Example.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Example = () => {
+  return (
+    <div>
+      <h1>Hello, I'm a functional component!</h1>
+      <p>I don't have any state or lifecycle methods.</p>
+    </div>
+  );
+};
+
+export default example;
