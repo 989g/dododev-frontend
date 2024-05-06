fe7212887a5079d6385297ccc2075962b7300fbc
989x <99growsup@gmail.com>
2024-05-06 14:17:22 +0700

    add components/layout/

--- my-app/src/app/contact/page.tsx
+++ my-app/src/app/contact/page.tsx
@@ -1,23 +1,21 @@
+import React from 'react';
+import Footer from "@/components/layout/Footer";
+import Navbar from "@/components/layout/Navbar";
+
 const ContactPage = () => {
   return (
-    <div className='container mx-auto px-4 py-8'>
-      <h1 className='text-3xl font-bold mb-4'>Contact Us</h1>
-      <p className='text-lg mb-4'>
-        This is the contact page. You can reach out to us through the following
-        methods:
-      </p>
-      <ul className='list-disc pl-4'>
-        <li className='mb-2'>
-          Phone: <span className='font-semibold'>123-456-7890</span>
-        </li>
-        <li className='mb-2'>
-          Email: <span className='font-semibold'>contact@example.com</span>
-        </li>
-        <li className='mb-2'>
-          Address:{' '}
-          <span className='font-semibold'>123 Main St, City, Country</span>
-        </li>
-      </ul>
+    <div>
+      <Navbar />
+      <div className="container mx-auto px-4 py-8">
+        <h1 className="text-3xl font-bold mb-4">Contact Us</h1>
+        <p className="text-lg mb-4">This is the contact page. You can reach out to us through the following methods:</p>
+        <ul className="list-disc pl-4">
+          <li className="mb-2">Phone: <span className="font-semibold">123-456-7890</span></li>
+          <li className="mb-2">Email: <span className="font-semibold">contact@example.com</span></li>
+          <li className="mb-2">Address: <span className="font-semibold">123 Main St, City, Country</span></li>
+        </ul>
+      </div>
+      <Footer />
     </div>
   );
 };
--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,9 +1,15 @@
-import Image from "next/image";
+import React from 'react';
+import Navbar from '../components/layout/Navbar';
+import Footer from '../components/layout/Footer';
 
 export default function Home() {
   return (
     <div>
-      <p className="p-3 text-lg font-bold text-blue-500">Welcome Home</p>
+      <Navbar />
+      <div className="container mx-auto px-4 py-8">
+        <p className="p-3 text-lg font-bold text-blue-500">Welcome Home</p>
+      </div>
+      <Footer />
     </div>
   );
 }
